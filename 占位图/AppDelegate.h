690ac2831b964117245bbf37c8229773845bc05f
jinshuaier <470401910@qq.com>
2017-09-13 15:22:25 +0800

    Initial Commit

---
+++ 占位图/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  占位图
+//
+//  Created by 胡高广 on 2017/9/13.
+//  Copyright © 2017年 jinshuaier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 占位图/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  占位图
+//
+//  Created by 胡高广 on 2017/9/13.
+//  Copyright © 2017年 jinshuaier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
